795f96c77e40418abbb8f5178da0b3a2d9834be8
Faith Ngwata <faithngwata059@gmail.com>
2026-09-22 20:11:29 +0530

    Added a code for time delay in lyrics.py

---
+++ lyrics.py
@@ -0,0 +1,12 @@
+import time
+
+lyrics = [
+ "I still got the feeling",
+ "Of your fingers on my skin",
+ "I'm meant to be healing"
+ "But I don't know whwere to begin"
+]
+
+for line in lyrics:
+    print(line)
+    time.sleep(4)
